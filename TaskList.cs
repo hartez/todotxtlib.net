dd5f787b554dfb5dc0a6bdd42b81c6ef97c6f07e
E.Z. Hart <hartez@gmail.com>
2011-05-13 11:28:41 -0600

    Updated gitignore; changed to 'using' statements for Stream management;

--- TaskList.cs
+++ TaskList.cs
@@ -1,311 +1,291 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 
 namespace todotxtlib.net
 {
-    public class TaskList : List<Task>
-    {
-        private String _numberFormat;
+	public class TaskList : List<Task>
+	{
+		private String _numberFormat;
 
-        public TaskList()
-        { }
+		public TaskList()
+		{
+		}
 
-        public TaskList(string filePath)
-        {
-            LoadTasks(filePath);
-        }
+		public TaskList(string filePath)
+		{
+			LoadTasks(filePath);
+		}
 
-        public TaskList(IEnumerable<Task> todos, int parentListItemCount)
-            : base(todos)
-        {
-            _numberFormat = new String('0', parentListItemCount.ToString().Length);
-        }
+		public TaskList(IEnumerable<Task> todos, int parentListItemCount)
+			: base(todos)
+		{
+			_numberFormat = new String('0', parentListItemCount.ToString().Length);
+		}
 
-        public IEnumerable<String> ToOutput()
-        {
-            return this.Select(x => x.ToString());
-        }
+		public IEnumerable<String> ToOutput()
+		{
+			return this.Select(x => x.ToString());
+		}
 
-        public IEnumerable<String> ToNumberedOutput()
-        {
-            if (String.IsNullOrEmpty(_numberFormat))
-            {
-                _numberFormat = new String('0', Count.ToString().Length);
-            }
+		public IEnumerable<String> ToNumberedOutput()
+		{
+			if (String.IsNullOrEmpty(_numberFormat))
+			{
+				_numberFormat = new String('0', Count.ToString().Length);
+			}
 
-            return this.Select(x => x.ToString(_numberFormat));
-        }
+			return this.Select(x => x.ToString(_numberFormat));
+		}
 
-        public TaskList ListCompleted()
-        {
-            return new TaskList(from todo in this
-                                where todo.Completed
-                                select todo, Count);
-        }
+		public TaskList ListCompleted()
+		{
+			return new TaskList(from todo in this
+			                    where todo.Completed
+			                    select todo, Count);
+		}
 
-        public TaskList Search(String term)
-        {
-            bool include = true;
+		public TaskList Search(String term)
+		{
+			bool include = true;
 
-            if (term.StartsWith("-"))
-            {
-                include = false;
-                term = term.Substring(1);
-            }
+			if (term.StartsWith("-"))
+			{
+				include = false;
+				term = term.Substring(1);
+			}
 
-            return new TaskList(from task in this
-                                where !(include ^ task.ToString().Contains(term, StringComparison.OrdinalIgnoreCase) )
-                                select task, Count);
-        }
+			return new TaskList(from task in this
+			                    where !(include ^ task.ToString().Contains(term, StringComparison.OrdinalIgnoreCase))
+			                    select task, Count);
+		}
 
-        public TaskList GetPriority(String priority)
-        {
-            if (!String.IsNullOrEmpty(priority))
-            {
-                return new TaskList(from todo in this
-                                    where todo.Priority == priority
-                                    select todo, Count);
-            }
-            
-            return new TaskList(from todo in this
-                                where todo.IsPriority
-                                orderby todo.Priority
-                                select todo, Count);
-        }
+		public TaskList GetPriority(String priority)
+		{
+			if (!String.IsNullOrEmpty(priority))
+			{
+				return new TaskList(from todo in this
+				                    where todo.Priority == priority
+				                    select todo, Count);
+			}
 
-        public void SetItemPriority(int item, string priority)
-        {
-            Task target = (from todo in this
-                           where todo.ItemNumber == item
-                           select todo).FirstOrDefault();
+			return new TaskList(from todo in this
+			                    where todo.IsPriority
+			                    orderby todo.Priority
+			                    select todo, Count);
+		}
 
-            if (target != null)
-            {
-                target.Priority = priority;
-            }
-        }
+		public void SetItemPriority(int item, string priority)
+		{
+			Task target = (from todo in this
+			               where todo.ItemNumber == item
+			               select todo).FirstOrDefault();
 
-        private bool ReplaceItemText(int item, string oldText, string newText)
-        {
-            Task target = (from todo in this
-                           where todo.ItemNumber == item
-                           select todo).FirstOrDefault();
+			if (target != null)
+			{
+				target.Priority = priority;
+			}
+		}
 
-            if (target != null)
-            {
-                return target.ReplaceItemText(oldText, newText);
-            }
+		private bool ReplaceItemText(int item, string oldText, string newText)
+		{
+			Task target = (from todo in this
+			               where todo.ItemNumber == item
+			               select todo).FirstOrDefault();
 
-            return false;
-        }
+			if (target != null)
+			{
+				return target.ReplaceItemText(oldText, newText);
+			}
 
-        public void ReplaceInTask(int item, string newText)
-        {
-            Task target = (from todo in this
-                           where todo.ItemNumber == item
-                           select todo).FirstOrDefault();
+			return false;
+		}
 
-            if (target != null)
-            {
-                target.Replace(newText);
-            }
-        }
+		public void ReplaceInTask(int item, string newText)
+		{
+			Task target = (from todo in this
+			               where todo.ItemNumber == item
+			               select todo).FirstOrDefault();
 
-        public void AppendToTask(int item, string newText)
-        {
-            Task target = (from todo in this
-                           where todo.ItemNumber == item
-                           select todo).FirstOrDefault();
+			if (target != null)
+			{
+				target.Replace(newText);
+			}
+		}
 
-            if (target != null)
-            {
-                target.Append(newText);
-            }
-        }
+		public void AppendToTask(int item, string newText)
+		{
+			Task target = (from todo in this
+			               where todo.ItemNumber == item
+			               select todo).FirstOrDefault();
 
-        public void PrependToTask(int item, string newText)
-        {
-            Task target = (from todo in this
-                           where todo.ItemNumber == item
-                           select todo).FirstOrDefault();
+			if (target != null)
+			{
+				target.Append(newText);
+			}
+		}
 
-            if (target != null)
-            {
-                target.Prepend(newText);
-            }
-        }
+		public void PrependToTask(int item, string newText)
+		{
+			Task target = (from todo in this
+			               where todo.ItemNumber == item
+			               select todo).FirstOrDefault();
 
-        public bool RemoveFromTask(int item, string term)
-        {
-            return ReplaceItemText(item, term, String.Empty);
-        }
+			if (target != null)
+			{
+				target.Prepend(newText);
+			}
+		}
 
-        public TaskList RemoveCompletedTasks(bool preserveLineNumbers)
-        {
-            TaskList completed = ListCompleted();
+		public bool RemoveFromTask(int item, string term)
+		{
+			return ReplaceItemText(item, term, String.Empty);
+		}
 
-            for (int n = Count - 1; n >= 0; n--)
-            {
-                if (this[n].Completed)
-                {
-                    if (preserveLineNumbers)
-                    {
-                        this[n].Empty();
-                    }
-                    else
-                    {
-                        Remove(this[n]);
-                    }
-                }
-            }
+		public TaskList RemoveCompletedTasks(bool preserveLineNumbers)
+		{
+			TaskList completed = ListCompleted();
 
-            return completed;
-        }
+			for (int n = Count - 1; n >= 0; n--)
+			{
+				if (this[n].Completed)
+				{
+					if (preserveLineNumbers)
+					{
+						this[n].Empty();
+					}
+					else
+					{
+						Remove(this[n]);
+					}
+				}
+			}
 
-        public void RemoveTask(int item, bool preserveLineNumbers)
-        {
-            Task target = (from todo in this
-                           where todo.ItemNumber == item
-                           select todo).FirstOrDefault();
+			return completed;
+		}
 
-            if (target != null)
-            {
-                if (preserveLineNumbers)
-                {
-                    target.Empty();
-                }
-                else
-                {
-                    Remove(target);
+		public void RemoveTask(int item, bool preserveLineNumbers)
+		{
+			Task target = (from todo in this
+			               where todo.ItemNumber == item
+			               select todo).FirstOrDefault();
 
-                    int itemNumber = 1;
-                    foreach (Task todo in this)
-                    {
-                        todo.ItemNumber = itemNumber;
-                        itemNumber += 1;
-                    }
-                }
-            }
-        }
+			if (target != null)
+			{
+				if (preserveLineNumbers)
+				{
+					target.Empty();
+				}
+				else
+				{
+					Remove(target);
 
-        public void LoadTasks(String filePath)
-        {
-            try
-            {
-                Clear();
+					int itemNumber = 1;
+					foreach (Task todo in this)
+					{
+						todo.ItemNumber = itemNumber;
+						itemNumber += 1;
+					}
+				}
+			}
+		}
 
-				var lines = ReadAllLines(filePath);
+		public void LoadTasks(String filePath)
+		{
+			try
+			{
+				Clear();
 
-                foreach (var line in lines)
-                {
-                    Add(new Task(line));
-                }
-            }
-            catch (IOException ex)
-            {
-                throw new TaskException("There was a problem trying to read from your todo.txt file", ex);
-            }
-        }
+				string[] lines = ReadAllLines(filePath);
 
-        public void SaveTasks(String filePath)
-        {
-            try
-            {
-                WriteAllLines(filePath, this.Select(t => t.ToString()).ToArray());
-            }
-            catch (IOException ex)
-            {
-                throw new TaskException("There was a problem trying to save your todo.txt file", ex);
-            }
-        }
+				foreach (string line in lines)
+				{
+					Add(new Task(line));
+				}
+			}
+			catch (IOException ex)
+			{
+				throw new TaskException("There was a problem trying to read from your todo.txt file", ex);
+			}
+		}
 
-        /// <summary>
-        /// Deletes a task from this list
-        /// </summary>
-        /// <param name="task">The task to delete from the list</param>
-        /// <returns>True if the task was in the list; false otherwise</returns>
-        public bool Delete(Task task)
-        {
-            try
-            {
-                return (Remove(this.First(t => t.Raw == task.Raw)));
-            }
-            catch (Exception ex)
-            {
-                throw new TaskException("An error occurred while trying to remove your task from the task list file", ex);
-            }
-        }
+		public void SaveTasks(String filePath)
+		{
+			try
+			{
+				WriteAllLines(filePath, this.Select(t => t.ToString()).ToArray());
+			}
+			catch (IOException ex)
+			{
+				throw new TaskException("There was a problem trying to save your todo.txt file", ex);
+			}
+		}
 
-        public void Update(Task currentTask, Task newTask)
-        {
-            try
-            {
-                var currentIndex = IndexOf(this.First(t => t.Raw == currentTask.Raw));
+		/// <summary>
+		/// Deletes a task from this list
+		/// </summary>
+		/// <param name="task">The task to delete from the list</param>
+		/// <returns>True if the task was in the list; false otherwise</returns>
+		public bool Delete(Task task)
+		{
+			try
+			{
+				return (Remove(this.First(t => t.Raw == task.Raw)));
+			}
+			catch (Exception ex)
+			{
+				throw new TaskException("An error occurred while trying to remove your task from the task list file", ex);
+			}
+		}
 
-                this[currentIndex] = newTask;
-            }
-            catch (Exception ex)
-            {
-                throw new TaskException("An error occurred while trying to update your task int the task list file", ex);
-            }
-        }
+		public void Update(Task currentTask, Task newTask)
+		{
+			try
+			{
+				int currentIndex = IndexOf(this.First(t => t.Raw == currentTask.Raw));
+
+				this[currentIndex] = newTask;
+			}
+			catch (Exception ex)
+			{
+				throw new TaskException("An error occurred while trying to update your task int the task list file", ex);
+			}
+		}
 
 		public static void WriteAllLines(string path, string[] lines)
 		{
-			FileStream fs = null;
-
-			try
+			using (var fs = File.Open(path, FileMode.Create, FileAccess.Write))
 			{
-				fs = File.Open(path, FileMode.Create, FileAccess.Write);
-
-				var sw = new StreamWriter(fs);
-
-				foreach (var line in lines)
+				using (var sw = new StreamWriter(fs))
 				{
-					sw.WriteLine(line);
-				}
+					foreach (string line in lines)
+					{
+						sw.WriteLine(line);
+					}
 
-				sw.Flush();
-			}
-			finally
-			{
-				if (fs != null)
-				{
-					fs.Close();
+					sw.Flush();
 				}
 			}
 		}
 
-    	public static string[] ReadAllLines(string path)
+		public static string[] ReadAllLines(string path)
 		{
-			FileStream fs = null;
 			var lines = new List<string>();
 
-			try
+			using (var fs = File.OpenRead(path))
 			{
-				fs = File.OpenRead(path);
-
-				var sr = new StreamReader(fs);
-				
-				string line = sr.ReadLine();
-				while(!String.IsNullOrEmpty(line))
+				using (var sr = new StreamReader(fs))
 				{
-					lines.Add(line);
-					line = sr.ReadLine();
-				}
-
-				return lines.ToArray();
-			}
-			finally
-			{
-				if (fs != null)
-				{
-					fs.Close();
+					string line = sr.ReadLine();
+					while (!String.IsNullOrEmpty(line))
+					{
+						lines.Add(line);
+						line = sr.ReadLine();
+					}
 				}
 			}
+			return lines.ToArray();
 		}
-    }
+	}
 }
